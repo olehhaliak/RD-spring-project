cba9de312bf846e21c8a9129bffb75d51b5ebd12
Oleh <olehhaliak@gmail.com>
2021-07-28 13:47:50 +0300

    initial commit

---
+++ src/main/java/my/flick/rd/hw3/RdLabHomework3Application.java
@@ -0,0 +1,13 @@
+package my.flick.rd.hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdLabHomework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdLabHomework3Application.class, args);
+    }
+
+}
---
+++ src/test/java/my/flick/rd/hw3/RdLabHomework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.flick.rd.hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdLabHomework3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
