29390ede6dbaee1f7f2e00524a4628ef25f10235
Oleh <olehhaliak@gmail.com>
2021-08-08 14:28:04 +0300

    added new exception handlers

--- src/main/java/my/flick/rd/springproject/controller/ExceptionHandlingController.java
+++ src/main/java/my/flick/rd/springproject/controller/ExceptionHandlingController.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import my.flick.rd.springproject.exception.NotFoundException;
 import my.flick.rd.springproject.exception.SelfReferencingException;
 import my.flick.rd.springproject.model.Error;
+import my.flick.rd.springproject.model.enums.ErrorType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,4 +31,13 @@ public class ExceptionHandlingController {
         return new ResponseEntity<>(new Error(exception),HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Error> handleException(MethodArgumentNotValidException exception){
+        return new ResponseEntity<>(new Error(exception.getMessage(), ErrorType.VALIDATION_ERROR_TYPE),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Error> handleException(HttpMessageNotReadableException exception){
+        return new ResponseEntity<>(new Error(exception.getMessage(), ErrorType.VALIDATION_ERROR_TYPE),HttpStatus.BAD_REQUEST);
+    }
 }
