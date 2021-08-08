db38e0c09ca3d5a2cf5306793a44944bae58e186
Oleh <olehhaliak@gmail.com>
2021-08-08 14:54:08 +0300

    added hateoas links for user

--- src/main/java/my/flick/rd/springproject/controller/assembler/UserAssembler.java
+++ src/main/java/my/flick/rd/springproject/controller/assembler/UserAssembler.java
@@ -6,6 +6,9 @@ import my.flick.rd.springproject.dto.UserDto;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
 public class UserAssembler extends RepresentationModelAssemblerSupport<UserDto, UserModel> {
     public UserAssembler() {
@@ -14,6 +17,12 @@ public class UserAssembler extends RepresentationModelAssemblerSupport<UserDto, 
 
     @Override
     public UserModel toModel(UserDto entity) {
-        return new UserModel(entity);
+        UserModel model = new UserModel(entity);
+        model.add(linkTo(methodOn(UserController.class).getUserById(entity.getId())).withSelfRel());
+        model.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("get all users"));
+        model.add(linkTo(methodOn(UserController.class).createUser(null)).withRel("add new user"));
+        model.add(linkTo(methodOn(UserController.class).updateUser(entity.getId(),null)).withRel("update user"));
+        model.add(linkTo(methodOn(UserController.class).deleteUser(entity.getId())).withRel("delete user"));
+        return model;
     }
 }
